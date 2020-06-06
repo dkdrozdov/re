17a1c0eba2bb231aa08a73b27a9b3557166a4676
dkdrozdov <dkdrozdov@gmail.com>
2020-06-06 15:58:38 +0700

    add buildFA method to Regex class

--- Regex.java
+++ Regex.java
@@ -1,11 +1,18 @@
 import java.util.*;
+
+import nfa.StateTable;
+import nfa.nfa_builder.NFABuilder;
 import parser.*;
 import parser.token.*;
 import parser.treebuilder.*;
 
 public class Regex {
+
+    public static StateTable buildFA(String regex) {
+        return NFABuilder.buildNFA(Treebuilder.buildTree(Parser.parse(regex)));
+    }
+
     public static void main(String[] args) {
-        ArrayList<Token> testTokens = Parser.parse("a*^b*^a^a^a^a");
-        Node<Token> testTree = Treebuilder.buildTree(testTokens);
+        StateTable testTable = buildFA("a*^a");
     }
 }
