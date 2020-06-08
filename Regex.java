a3537f79ef603d1124249db2afd806a60206bf49
dkdrozdov <dkdrozdov@gmail.com>
2020-06-08 23:31:51 +0700

    change method buildFA implementation

--- Regex.java
+++ Regex.java
@@ -1,18 +1,21 @@
-import java.util.*;
+import java.util.List;
 
 import nfa.StateTable;
 import nfa.nfa_builder.NFABuilder;
 import parser.*;
-import parser.token.*;
+import parser.token.Token;
 import parser.treebuilder.*;
 
 public class Regex {
 
     public static StateTable buildFA(String regex) {
-        return NFABuilder.buildNFA(Treebuilder.buildTree(Parser.parse(regex)));
+        List<Token> tokenArray = Parser.parse(regex);
+        Node<Token> treeRoot = Treebuilder.buildTree(tokenArray);
+        StateTable table = NFABuilder.buildNFA(treeRoot);
+        return table;
     }
 
     public static void main(String[] args) {
-        StateTable testTable = buildFA("a*^a");
+        StateTable testTable = buildFA("c^b*");
     }
 }
