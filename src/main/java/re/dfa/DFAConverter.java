6e4999fd68fd0afee766b0734c601db9fa7b320f
dkdrozdov <dkdrozdov@gmail.com>
2020-06-10 17:42:29 +0700

    add DFAConverter class

---
+++ src/main/java/re/dfa/DFAConverter.java
@@ -0,0 +1,38 @@
+package re.dfa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import re.fa.SpecialTransitions;
+import re.fa.StateTable;
+
+public class DFAConverter {
+    public static StateTable convertToDFA(StateTable table) {
+        StateTable DFATable = new StateTable();
+        DFATable = eliminateNonDeterminism(table, table.getStartState());
+        return new StateTable();
+    }
+
+    public static List<Integer> buildEpsilonClosure(StateTable table, int startState) {
+        List<Integer> closureStates = new ArrayList<Integer>();
+        closureStates.add(startState);
+        int freeTransLitIndex = table.inputLits.indexOf(SpecialTransitions.freeTransition);
+        if (freeTransLitIndex != -1) {
+            for (int freeTransition = 0; freeTransition < table.stateTable.get(startState).get(freeTransLitIndex)
+                    .size(); freeTransition++) {
+                closureStates.addAll(buildEpsilonClosure(table,
+                        table.stateTable.get(startState).get(freeTransLitIndex).get(freeTransition)));
+            }
+        }
+        return closureStates;
+    }
+
+    public static StateTable eliminateNonDeterminism(StateTable table, int startState) {
+        List<Integer> states = new ArrayList<Integer>();
+        // add in states list startState and all states which startState lead to through
+        // free transition
+        states = buildEpsilonClosure(table, startState);
+
+        return null;
+    }
+}
