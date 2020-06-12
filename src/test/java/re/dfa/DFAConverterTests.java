e819c6a44cccbb835183c464d0cf825e1cdc88a2
dkdrozdov <dkdrozdov@gmail.com>
2020-06-12 21:45:55 +0700

    add eliminateNonDeterminism test

--- src/test/java/re/dfa/DFAConverterTests.java
+++ src/test/java/re/dfa/DFAConverterTests.java
@@ -8,6 +8,101 @@ import org.junit.Test;
 import re.fa.StateTable;
 
 public class DFAConverterTests {
+
+    @Test
+    public void eliminateNonDeterminismTest() {
+        // set up test table
+        StateTable table = new StateTable();
+        table.addInputLit("a");
+        table.addInputLit("b");
+        table.addInputLit("c");
+        table.addInputLit("0");
+        table.addInputLit("1");
+        table.addInputLit("2");
+        table.addInputLit("3");
+        table.addInputLit(".");
+        for (int i = 0; i < 18; i++) {
+            table.addState();
+        }
+        table.addTransition("a", 0, 1);
+        table.addTransition("b", 1, 2);
+        table.addTransition("b", 2, 2);
+        table.addFreeTransition(1, 3);
+        table.addFreeTransition(2, 3);
+        table.addFreeTransition(3, 19);
+        table.addTransition("c", 0, 4);
+        table.addTransition("c", 0, 9);
+        table.addTransition("c", 4, 4);
+        table.addTransition("c", 9, 9);
+        table.addFreeTransition(0, 10);
+        table.addFreeTransition(9, 10);
+        table.addFreeTransition(4, 5);
+        table.addFreeTransition(5, 19);
+        table.addTransition(".", 5, 6);
+        table.addTransition(".", 10, 11);
+        table.addTransition("c", 11, 12);
+        table.addTransition("c", 12, 12);
+        table.addTransition("c", 6, 7);
+        table.addTransition("c", 7, 7);
+        table.addTransition("c", 11, 12);
+        table.addFreeTransition(6, 8);
+        table.addFreeTransition(12, 13);
+        table.addFreeTransition(7, 8);
+        table.addFreeTransition(8, 19);
+        table.addFreeTransition(13, 19);
+        table.addTransition("0", 0, 14);
+        table.addFreeTransition(14, 19);
+        table.addTransition("1", 0, 15);
+        table.addFreeTransition(15, 19);
+        table.addTransition("2", 0, 16);
+        table.addFreeTransition(16, 19);
+        table.addTransition("3", 0, 17);
+        table.addFreeTransition(17, 19);
+        table.setFinalState(19);
+        // set up expected table
+        StateTable expectedTable = new StateTable();
+        for (int i = 0; i < 11; i++) {
+            expectedTable.addState();
+        }
+        expectedTable.addInputLit("b");
+        expectedTable.addFreeTransition(2, 3);
+        expectedTable.addInputLit("a");
+        expectedTable.addInputLit("c");
+        expectedTable.addInputLit(".");
+        expectedTable.addInputLit("0");
+        expectedTable.addInputLit("1");
+        expectedTable.addInputLit("2");
+        expectedTable.addInputLit("3");
+        expectedTable.addTransition("a", 0, 1);
+        expectedTable.addTransition("c", 0, 4);
+        expectedTable.addTransition(".", 0, 11);
+        expectedTable.addTransition("0", 0, 7);
+        expectedTable.addTransition("1", 0, 8);
+        expectedTable.addTransition("2", 0, 9);
+        expectedTable.addTransition("3", 0, 10);
+        expectedTable.addTransition("b", 1, 2);
+        expectedTable.addFreeTransition(1, 3);
+        expectedTable.addTransition("b", 2, 2);
+        expectedTable.addTransition("c", 4, 4);
+        expectedTable.addTransition(".", 4, 5);
+        expectedTable.addFreeTransition(4, 3);
+        expectedTable.addTransition("c", 5, 6);
+        expectedTable.addFreeTransition(5, 3);
+        expectedTable.addTransition("c", 6, 6);
+        expectedTable.addFreeTransition(6, 3);
+        expectedTable.addFreeTransition(7, 3);
+        expectedTable.addFreeTransition(8, 3);
+        expectedTable.addFreeTransition(9, 3);
+        expectedTable.addFreeTransition(10, 3);
+        expectedTable.addTransition("c", 11, 12);
+        expectedTable.addTransition("c", 12, 12);
+        expectedTable.addFreeTransition(12, 3);
+        expectedTable.setFinalState(3);
+        // set up actual table
+        StateTable actualTable = DFAConverter.eliminateNonDeterminism(table);
+        assertEquals(expectedTable, actualTable);
+    }
+
     @Test
     public void buildEpsilonClosureTest() {
         // set up testTable
