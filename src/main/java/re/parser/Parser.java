9608a7f82377cb7a5cc634762ee146a81f7381a5
dkdrozdov <dkdrozdov@gmail.com>
2020-06-14 21:33:19 +0700

    add insertConcatenations method for Parser class

--- src/main/java/re/parser/Parser.java
+++ src/main/java/re/parser/Parser.java
@@ -4,8 +4,8 @@ import java.util.*;
 import re.parser.token.*;
 
 public class Parser {
-    public static ArrayList<Token> parse(String s) {
-        ArrayList<Token> tokens = new ArrayList<Token>();
+    public static List<Token> parse(String s) {
+        List<Token> tokens = new ArrayList<Token>();
         Token nextToken;
         for (int i = 0; i < s.length(); i++) {
             switch (s.charAt(i)) {
@@ -23,6 +23,37 @@ public class Parser {
             }
             tokens.add(nextToken);
         }
+        tokens = insertConcatenations(tokens);
+        return tokens;
+    }
+
+    public static List<Token> insertConcatenations(List<Token> initialTokens) {
+        List<Token> tokens = new ArrayList<Token>();
+
+        for (int i = 0; i < initialTokens.size(); i++) {
+            tokens.add(initialTokens.get(i));
+            if (i != initialTokens.size() - 1) {
+                TokenType tokenType = initialTokens.get(i).getType();
+                switch (tokenType) {
+                    case LITERAL: {
+                        if (initialTokens.get(i + 1).getType() == TokenType.LITERAL) {
+                            tokens.add(new Concat());
+                        }
+                        break;
+                    }
+                    case ASTERISK: {
+                        if (initialTokens.get(i + 1).getType() == TokenType.LITERAL) {
+                            tokens.add(new Concat());
+                        }
+                        break;
+                    }
+                    default: {
+
+                    }
+                }
+            }
+        }
+
         return tokens;
     }
 }
