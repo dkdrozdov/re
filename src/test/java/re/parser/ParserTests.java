9e03f5db0ff4dec7154037e9d3758de80a6d0967
dkdrozdov <dkdrozdov@gmail.com>
2020-06-14 21:33:48 +0700

    add insertConcatenations test

---
+++ src/test/java/re/parser/ParserTests.java
@@ -0,0 +1,38 @@
+package re.parser;
+
+import static org.junit.Assert.assertEquals;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import re.parser.token.*;
+import re.parser.token.Token;
+
+public class ParserTests {
+    @Test
+    public void insertConcatenationsTest() {
+        // set up actual tokens
+        List<Token> actual = new ArrayList<Token>();
+        actual.add(new Literal("a"));
+        actual.add(new Asterisk());
+        actual.add(new Literal("b"));
+        actual.add(new Literal("c"));
+        actual.add(new Literal("d"));
+        actual.add(new Asterisk());
+        actual = Parser.insertConcatenations(actual);
+        // set up expected tokens
+        List<Token> expected = new ArrayList<Token>();
+        expected.add(new Literal("a"));
+        expected.add(new Asterisk());
+        expected.add(new Concat());
+        expected.add(new Literal("b"));
+        expected.add(new Concat());
+        expected.add(new Literal("c"));
+        expected.add(new Concat());
+        expected.add(new Literal("d"));
+        expected.add(new Asterisk());
+
+        assertEquals(expected, actual);
+    }
+}
