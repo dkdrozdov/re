039e1562508adaf6fc27cf45fe059ae0735bdf35
dkdrozdov <dkdrozdov@gmail.com>
2020-06-14 18:56:48 +0700

    add otherTransition to SpecialTransitions

--- src/main/java/re/fa/SpecialTransitions.java
+++ src/main/java/re/fa/SpecialTransitions.java
@@ -2,4 +2,5 @@ package re.fa;
 
 public class SpecialTransitions {
     public static final String freeTransition = "freeTransition";
+    public static final String otherTransition = "otherTransition";
 }
